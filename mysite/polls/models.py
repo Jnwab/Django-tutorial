5f9937c697165466a15968e212b6e529e2c41fcb
Joshua Nwabuzor <buddyfight45@gmaiil.com>
2021-06-24 10:24:16 -0700

    Created Database & SuperAdmin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+
+import datetime
+
+from django.db import models #importing models, simple.
+from django.utils import timezone #IMported timezone, like  actual TIME managment.
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200) #CharField is meant to just represent strings taht are written out.
+    pub_date = models.DateTimeField('date published') #DateTimeField seems to be the storage of data
+    
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >+ timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
