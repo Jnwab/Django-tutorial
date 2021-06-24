bcf3f487a628ee8bb4cc7b936c9302f288d74315
Joshua Nwabuzor <buddyfight45@gmaiil.com>
2021-06-24 11:59:56 -0700

    Day 2:SuperAdmin, Day 3: Views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views # the '.' means from ALL accesable directories, you're looking into it
 
 urlpatterns = [
-    path('',views.index, name='index') # You're url is a list of path functions, kinda wild.
+    path('',views.index, name='index'), # You're url is a list of path functions, kinda wild.
+    path('<int:question_id>/', views.detail,name="detail"), # Polls path that allows you to view the question  NUMBER
+    path('<int:question_id>/results/',views.results, name="results"), # Polls path allowing u to view the results of the question you answered
+    path('<int:question_id>/resuts/',views.vote, name="vote") #polls path letting you see/choices and choosing one.
+
 ]
 
 #make sure all urls are going/pointed in the correct area
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,20 @@
+from django import http
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 
-
+# This is how all that data is visually shown and accsed through request and responses
+#Templates are really aiming  the website engaging to the user, seprating how the data is being SHOWN from what the data is actually.
 def index(request): #A request,  which allows you to see the webpages we see today
     return HttpResponse("Hello, world. You're at the polls index.") #If the response is accepted, well you'll see Hello world.
 
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of of question %s."
+    return HttpResponse(response%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+#After writing these, you have to wire them back towards the urls path polls/url.py
