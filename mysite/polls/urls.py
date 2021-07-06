62e3978cd8584616750845f02e5f9ad03cf3b1ca
Joshua Nwabuzor <buddyfight45@gmaiil.com>
2021-07-06 12:28:21 -0700

    Django Testing days!

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from . import views # the '.' means from ALL accesable directories, you're looking into it
-
+app_name = "polls"
 urlpatterns = [
-    path('',views.index, name='index'), # You're url is a list of path functions, kinda wild.
-    path('<int:question_id>/', views.detail,name="detail"), # Polls path that allows you to view the question  NUMBER
-    path('<int:question_id>/results/',views.results, name="results"), # Polls path allowing u to view the results of the question you answered
-    path('<int:question_id>/resuts/',views.vote, name="vote") #polls path letting you see/choices and choosing one.
-
+    #Example: /polls/
+    path('',views.IndexView.as_view(), name='index'), # You're url is a list of path functions, kinda wild.
+    #Example: /polls/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # Polls path that allows you to view the question  NUMBER
+    #Example polls/5/results
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), # Polls path allowing u to view the results of the question you answered
+    #Example polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'), #polls path letting you see/choices and choosing one.
+    
 ]
 
 #make sure all urls are going/pointed in the correct area
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,59 @@
 from django import http
-from django.shortcuts import render
-from django.http import HttpResponse, response
+from django import template
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, request, response,HttpResponseRedirect
+from django.template import context, loader
+from django.http import Http404
+from django.urls import reverse
+from  django.views import generic
+from .models import Choice, Question
+from django.utils import timezone
+
 
 # This is how all that data is visually shown and accsed through request and responses
 #Templates are really aiming  the website engaging to the user, seprating how the data is being SHOWN from what the data is actually.
-def index(request): #A request,  which allows you to see the webpages we see today
-    return HttpResponse("Hello, world. You're at the polls index.") #If the response is accepted, well you'll see Hello world.
+#def index(request): #A request,  which allows you to see the webpages we see today
+    #return HttpResponse("Hello, world. You're at the polls index.") #If the response is accepted, well you'll see Hello world.
 
-# Create your views here.
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You're looking at the results of of question %s."
-    return HttpResponse(response%question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(
+            pub_date_lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# ...
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 #After writing these, you have to wire them back towards the urls path polls/url.py
